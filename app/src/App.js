9eecbc8ea2d6a6236a423fd6d6672a37ded26eea
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-21 13:18:11 -0300

    feat: using the solana object

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,32 @@ const TWITTER_HANDLE = 'vit0rrk';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = async()=>{
+    try {
+      const {solana} = window
+
+      if (solana){
+        if (solana.isPhantom){
+          console.log('Phantom wallet found!')
+
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet')
+      }
+    } catch (error){
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected()
+    }
+    window.addEventListener('load', onLoad)
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
