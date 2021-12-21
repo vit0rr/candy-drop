e79e32528903f68f84273b31659b2e24eabaf768
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-21 13:18:30 -0300

    feat: accessing the user's account

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,8 @@ const App = () => {
       if (solana){
         if (solana.isPhantom){
           console.log('Phantom wallet found!')
-
+          const response = await solana.connect({onlyIfTrusted: true})
+          console.log('Connected with Public Key:', response.publicKey.toString())
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet')
