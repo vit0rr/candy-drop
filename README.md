7d362710adb77673cf40474b413c7c9ffb11559f
Vitor <vi.souza.almeida@protonmail.com>
2022-01-01 17:30:36 -0300

    clear

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-### Building a mint NFT with Solana
+# buildspace Solana NFT Drop Project
+### Welcome 👋
+To get started with this course, clone this repo and follow these commands:
 
-I will create a web app that lets users connect their wallets, click mint, and receive an NFT from our collection in their wallet.
+1. cd into the `app` folder
+2. Run `npm install` at the root of your directory
+3. Run `npm run start` to start the project
+4. Start coding!
 
-### Candy Machine
+### What is the .vscode Folder?
+If you use VSCode to build your app, we included a list of suggested extensions that will help you build this project! Once you open this project in VSCode, you will see a popup asking if you want to download the recommended extensions :).
 
-Candy Machine is what Metaplex cals a basic NFT drop where users can come in, click mint, and get an NFT
+### Questions?
+Have some questions make sure you head over to your [buildspace Dashboard](https://app.buildspace.so/projects/CO77556be5-25e9-49dd-a799-91a2fc29520e) and link your Discord account so you can get access to helpful channels and your instructor!
+
--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
@@ -25,7 +25,6 @@ const MAX_SYMBOL_LENGTH = 10;
 const MAX_CREATOR_LEN = 32 + 1 + 1;
 
 const CandyMachine = ({ walletAddress }) => {
-  const [machineStats, setMachineStats] = useState(null);
   // Actions
   const fetchHashTable = async (hash, metadataEnabled) => {
     const connection = new web3.Connection(
@@ -251,84 +250,15 @@ const CandyMachine = ({ walletAddress }) => {
     });
   };
 
-  useEffect(() => {
-    getCandyMachineState();
-  }, []);
-
-  const getProvider = () => {
-    const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
-    // Create a new connection object
-    const connection = new Connection(rpcHost);
-    
-    // Create a new Solana provider object
-    const provider = new Provider(
-      connection,
-      window.solana,
-      opts.preflightCommitment
-    );
-  
-    return provider;
-  };
-
-  const getCandyMachineState = async () => { 
-    const provider = getProvider();
-    
-    // Get metadata about your deployed candy machine program
-    const idl = await Program.fetchIdl(candyMachineProgram, provider);
-  
-    // Create a program that you can call
-    const program = new Program(idl, candyMachineProgram, provider);
-  
-    // Fetch the metadata from your candy machine
-    const candyMachine = await program.account.candyMachine.fetch(
-      process.env.REACT_APP_CANDY_MACHINE_ID
-    );
-    
-    // Parse out all our metadata and log it out
-    const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
-    const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
-    const itemsRemaining = itemsAvailable - itemsRedeemed;
-    const goLiveData = candyMachine.data.goLiveDate.toNumber();
-  
-    // We will be using this later in our UI so let's generate this now
-    const goLiveDateTimeString = `${new Date(
-      goLiveData * 1000
-    ).toLocaleDateString()} @ ${new Date(
-      goLiveData * 1000
-    ).toLocaleTimeString()}`;
-
-    setMachineStats({
-      itemsAvailable,
-      itemsRedeemed,
-      itemsRemaining,
-      goLiveData,
-      goLiveDateTimeString,
-    });
-  
-    console.log({
-      itemsAvailable,
-      itemsRedeemed,
-      itemsRemaining,
-      goLiveData,
-      goLiveDateTimeString,
-    });
-  };
-
   return (
-    machineStats && (
-      <div className="machine-container">
-    <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>
-    <p>{`Items Minted: ${machineStats.itemsRedeemed} / ${machineStats.itemsAvailable}`}</p>
-    <button className="cta-button mint-button" onClick={null}>
+    <div className="machine-container">
+      <p>Drop Date:</p>
+      <p>Items Minted:</p>
+      <button className="cta-button mint-button" onClick={mintToken}>
         Mint NFT
-    </button>
-  </div>
-    )
+      </button>
+    </div>
   );
-
-
 };
-
-
 
 export default CandyMachine;
